4a0dc3163edf75601a2131ac1dfaacca45c950b1
Nadrojria <loicdesbordes@yahoo.fr>
2025-06-02 14:19:11 +0200

    add: login localstorage and logout

--- src/script.js
+++ src/script.js
@@ -117,6 +117,7 @@ const cards = {
                     <span class="text-sm font-medium">Update</span>
                 </button>
                 <button type="submit"
+                    id="log-out-btn" 
                     class="log-out-btn flex items-center justify-center gap-2 bg-my-black hover:bg-my-black transition text-white px-4 py-2 rounded-lg self-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -207,6 +208,8 @@ function showCard(name) {
             
             if (logInData.success) {
                 currentUser = logInData.firstName;
+                localStorage.setItem("currentUser", JSON.stringify(currentUser));
+
                 showCard('dashboard');
                 navbar.style.display = "block";
                 const adminButton = document.querySelector("#admin-btn");
@@ -218,6 +221,15 @@ function showCard(name) {
             else {
                 alert("Error! Check Info.");
             }
+        })
+    }
+
+    if (name === "profile") {
+        const logOutBtn = document.querySelector("#log-out-btn");
+        logOutBtn.addEventListener("click", () => {
+            localStorage.removeItem ("currentUser");
+            navbar.style.display = "none";
+            showCard("signIn");
         })
     }
 }
@@ -236,10 +248,26 @@ function animateNavbar(name) {
     });
 }
 
+function updateLocalStorage() {
+    const currentUserString = localStorage.getItem("currentUser");
+    if (currentUserString) {
+        currentUser = JSON.parse(currentUserString);
+    }
+}
+
 
 // execute code
 
 document.addEventListener('DOMContentLoaded', function () {
-    navbar.style.display = "none";
-    showCard('signIn');
+
+    updateLocalStorage();
+    
+    if(currentUser) {
+        showCard("dashboard");
+    } else {
+        navbar.style.display = "none";
+        showCard('signIn');
+    }
+
 });
+
